3cc21eef50a16e734746ff18b85f09581b226442
Muneeb-hub411 <muneebsaleem402@gmail.com>
2024-05-25 17:00:50 +0500

    box designing flexbox etc

---
+++ components/Section_1.tsx
@@ -0,0 +1,58 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const Section_1 = () => {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.YellowColor}>
+          <Text style={styles.BoxText}>Yellow Box</Text>
+        </View>
+        <View style={styles.blueColor}>
+          <Text style={styles.BoxText}>Blue Box</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Section_1;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 28,
+    paddingVertical: 28,
+  },
+  BoxText: {
+    textAlign: 'center',
+    color: '#a9a9a9',
+  },
+  YellowColor: {
+    flex: 0,
+    justifyContent: 'center',
+    textAlign: 'center',
+    margin: 4,
+    backgroundColor: '#ffff00',
+    borderWidth: 3,
+    borderColor: '#fffaf0',
+    borderRadius: 15,
+    height: 100,
+    width: 100,
+  },
+  blueColor: {
+    flex: 0,
+    justifyContent: 'center',
+    margin: 4,
+    backgroundColor: '#00bfff',
+    borderWidth: 3,
+    borderColor: '#fffaf0',
+    borderRadius: 15,
+    height: 100,
+    width: 100,
+  },
+});
